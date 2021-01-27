52773d818efb039d8fcfd9d789ce1ba4801b9de7
dvel <dvel@atlante>
2021-01-27 20:21:36 +0100

    Added bootstrap, autosuggest, used the original example from autosuggest page https://github.com/moroshko/react-autosuggest

--- client/multiselector/src/components/HomeComponent.js
+++ client/multiselector/src/components/HomeComponent.js
@@ -1,14 +1,17 @@
 
 import React from 'react';
-
+import Example from './AutoSuggestExample';
 
 function Home(props) {
 	return (
 		<div className="container">
-            <div className="row align-items-start">
-		        <div className="col-12 col-md m-1">
-                    <h1>CIAO</h1>
-		        </div>
+            <div className="row justify-content-md-center">
+		        <div className="col-6">
+                    <Example />
+                </div>
+                <div className="col-6">
+                    <Example />
+                </div>
             </div>
         </div>
 	);
